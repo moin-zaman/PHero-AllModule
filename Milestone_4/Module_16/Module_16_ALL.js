b0835aeaac9b6d695045c98856f3cd6749a49003
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-08 00:00:35 +0600

    Milestone 4 Folder Added and Module 16 completed

---
+++ Milestone_4/Module_16/Module_16_ALL.js
@@ -0,0 +1,31 @@
+console.log(3342333);
+
+var price = 123;
+var name = "Moin";
+var isNull = true;
+
+console.log(typeof(price));
+console.log(typeof(name));
+console.log(typeof(isNull));
+
+var a = "54.25";
+var b = "2.65";
+
+a = parseFloat(a);
+b = parseFloat(b);
+
+var sum = a + b;
+
+console.log(sum);
+
+var c = 0.345253;
+var d = 0.23;
+
+var sum1 = c + d;
+
+sum1 = sum1.toFixed(5);
+
+sum1 = parseFloat(sum1);
+
+console.log(sum1);
+console.log(typeof(sum1));
