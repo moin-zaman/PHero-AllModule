9ad6e4d5a3772d5949278bf24c3276c7b7edd8c9
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-08 06:57:49 +0600

    Number toString toFixed toPrecision Method

--- Milestone_4/Anisul_Islam_Js/index.js
+++ Milestone_4/Anisul_Islam_Js/index.js
@@ -1,1 +1,32 @@
 document.write("Hello This is Moin");
+
+var num = 20;
+
+        document.write(typeof(num),"<br/>");
+
+        var num1 = toString(num);
+        document.write(typeof(num1),"<br/>");
+
+        var name = "Moin";
+
+        var name1 = parseInt(name);
+        document.write(typeof(name1));
+        document.write("<br/>",name1,"<br/>");
+
+        var n;
+        document.write(n,"<br/>");
+
+        document.write("<br/> <br/>")
+
+        var number = 2.45353;
+
+        document.write(number, "<br/>");
+
+        number = number.toFixed(3);
+        document.write(number);
+
+        // var number1 = number.toPrecision(5);
+        // document.write(number1, "<br/>");
+
+        var number1 = 23;
+        document.write(number1);
