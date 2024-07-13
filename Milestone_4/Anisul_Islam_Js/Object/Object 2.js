06ca20af867de96bed67213d89d080880955e77e
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 06:13:34 +0600

    Object 2.js File Updated

--- Milestone_4/Anisul_Islam_Js/Object/Object 2.js
+++ Milestone_4/Anisul_Islam_Js/Object/Object 2.js
@@ -5,8 +5,10 @@ function Demo(text) {
 }
 
 const result = new Demo("Bangladesh");
+result.name = "moin";
 console.log(result.country); // Outputs: undefined
 console.log(result.name);    // Outputs: undefined
 console.log(result.edu);     // Outputs: akcc
+console.log(result);
 
 // Here, result is the object { edu: "akcc" } returned by the constructor, and it does not have the country and name properties assigned to this within the constructor.
