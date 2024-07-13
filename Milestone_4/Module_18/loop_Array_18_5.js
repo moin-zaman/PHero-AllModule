684f948eef15a91192032a0d02f3829f9a95d965
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 11:35:46 +0600

    Loop in Array Added 18_5

---
+++ Milestone_4/Module_18/loop_Array_18_5.js
@@ -0,0 +1,16 @@
+// node loop_Array_18_5.js
+
+var num = [20, 30, 40, 60, 80, 90];
+
+for(var x = 0; x < num.length; x = x + 1)
+{
+    // console.log(num);
+    console.log(num[x]);
+}
+
+var name = ["Moin", "Zaman", "Khan"];
+
+for(var x = 0; x < name.length; x = x+1)
+{
+    console.log(name[x]);
+}
