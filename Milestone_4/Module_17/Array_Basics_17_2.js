15ed2af7d57d634bb570c69401c5837546310f02
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 06:52:12 +0600

    Array Basics !7_2 Added

---
+++ Milestone_4/Module_17/Array_Basics_17_2.js
@@ -0,0 +1,21 @@
+var name = new Array(3);
+        name[0] = "Moin";
+        name[1] = "Zaman";
+        name[2] = "Khan";
+    
+        console.log("name[0] = " + name[0]);
+        console.log("name[1] = " + name[1]);
+        console.log("name[2] = " + name[2]);
+        console.log(name);
+
+        var name1 = ["Moin" , "Zaman" ,"Khan"];
+        console.log(name1);
+
+        var charac = ["a", "b", "c", "d"];
+        console.log(charac);
+
+        var nam = "Moin";
+        var nam1 = nam.charAt(2);
+        console.log(nam1);
+
+        // node "Array_Basics_17_2.js"
