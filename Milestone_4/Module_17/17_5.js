3260a88d00bdc98070a184332c195c8268fe6a48
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 07:36:05 +0600

    17_5 File Added

---
+++ Milestone_4/Module_17/17_5.js
@@ -0,0 +1,8 @@
+// Relational Operator
+// ==, ===, !=, !==, <, <!, >, >!, <=, <=!, >=, >=!
+
+console.log(5 >=! 6);
+console.log(5 <! 6);
+
+console.log(5 < 6 && 5 < 2);
+console.log(5 < 6 || 5 > 2);
