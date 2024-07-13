789de90743becdb1b6f587abcf11ca13d541cfce
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 08:04:07 +0600

    17_6 Updated

--- Milestone_4/Module_17/conditional_17_6.js
+++ Milestone_4/Module_17/conditional_17_6.js
@@ -11,8 +11,30 @@ else if(n2 > n3)
 {
     console.log(n2);
 }
-
 else
 {
     console.log(n3);
+}
+
+var gr = true;
+var sal = 80000;
+var car = 1;
+
+if(gr === true && sal >= 100000)
+{
+    console.log("Biya kor beda");
+}
+else
+{
+
+    console.log("Thak Biya korar ddorkar nai");
+} 
+
+if(gr === true && (sal > 100000 || car >= 1))
+{
+    console.log("Taratari biya koren");
+}
+else
+{
+    console.log("Biya Putki da dimu");
 }
