5a057e3937a1869d954af8ce90271c34a5d34dd9
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 07:43:07 +0600

    control Statement 17_6 Added

---
+++ Milestone_4/Module_17/conditional_17_6.js
@@ -0,0 +1,18 @@
+//  node "conditional_17_6.js"
+
+var n1 = 10;
+var n2 = 18;
+var n3 = 21;
+if(n1 > n2 && n2 > n3)
+{
+    console.log(n1);
+}
+else if(n2 > n3)
+{
+    console.log(n2);
+}
+
+else
+{
+    console.log(n3);
+}
