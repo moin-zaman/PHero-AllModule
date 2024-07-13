3bd2749df6cf57180aa376c9fa3556ba3c77c413
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-13 11:15:51 +0600

    While Loop and Example 18_2 and 18_3 Added

---
+++ Milestone_4/Module_17/if_else_part_2.js
@@ -0,0 +1,23 @@
+var n1 = 5, n2 = 20, n3 = 15;
+if(n1 > n2)
+{
+    if(n1 > n3)
+    {
+        console.log(n1);
+    }
+    else
+    {
+        console.log(n3);
+    }
+}
+else
+{
+    if(n2 > n3)
+    {
+        console.log(n2);
+    }
+    else
+    {
+        console.log(n3);
+    }
+}
---
+++ Milestone_4/Module_18/while_loop_18_2.js
@@ -0,0 +1,42 @@
+var roastGiven = 0;
+while(roastGiven < 7)
+{
+    console.log("Roast Den Please ");
+    roastGiven +=1;
+}
+
+var num = 0;
+
+while(num <= 10)
+{
+    if(num % 2  === 0)
+    {
+        console.log(num)
+    }
+    num = num + 1;
+}
+
+var num1 = 1;
+
+
+while(num1 <= 10)
+    {
+        
+        console.log(num1)
+        num1 = num1 + 2;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// node while_loop_18_2.js
