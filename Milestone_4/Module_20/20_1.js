ff78cdcab6631f9a88277f18223085151c70477c
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-03 16:47:12 +0600

    Module 20_1 Added

---
+++ Milestone_4/Module_20/20_1.js
@@ -0,0 +1,17 @@
+// We can create string value using Back tick"
+
+const district = `Narsingdi`;
+const thana  = new String("Narsingdi Sadar");
+
+console.log(district);
+console.log(typeof (district));
+
+console.log(thana);
+console.log(typeof (thana));
+
+console.log(district.length);
+console.log(district[3]);
+
+// String is Immutable
+
+// district[4] = 'f'; Not Possible
