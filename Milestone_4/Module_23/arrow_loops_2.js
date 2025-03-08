de75594ab532db69664b7454a0741eb7c694324d
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-08 08:59:51 +0600

    Added arrow loops

---
+++ Milestone_4/Module_23/arrow_loops_2.js
@@ -0,0 +1,34 @@
+// function display1(){
+//     console.log("I am display 1");}
+
+// const display2 = () => {console.log("I am display 2")};
+// const display3 = () => console.log("I am display 3");
+
+// display1();  
+// display2();  
+// display3();  
+
+
+function msg1(){
+    return "Hi am message 1";
+}
+console.log(msg1());
+
+
+
+// Task 2
+
+// const msg2 = () => {
+//     return "Hi am message 2";
+// }
+
+const msg2 = () => "Hi am message 2";
+console.log(msg2());
+
+
+// Task3
+
+
+
+const add2 = (num1, num2) => num1 + num2;
+console.log(add2(2,5));
