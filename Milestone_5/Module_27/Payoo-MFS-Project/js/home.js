fa7690552f60f1f76aee01086081ab05e1f15518
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-19 12:02:40 +0600

    Add money and Cash out Used by JS 27_6

--- Milestone_5/Module_27/Payoo-MFS-Project/js/home.js
+++ Milestone_5/Module_27/Payoo-MFS-Project/js/home.js
@@ -1,1 +1,66 @@
-console.log("Home Added");
+const inputAddMoney = document.getElementById('input-add-money');
+
+const inputPinNumber = document.getElementById('input-pin-number');
+
+const addMoneyBtn = document.getElementById('btn-add-money');
+
+const cashOutBtn = document.getElementById('btn-show-cash-out');
+
+const accountBalance = document.getElementById('account-balance');
+
+
+addMoneyBtn.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const valueAddMoney = parseFloat(inputAddMoney.value);
+    const valuePinNumber = inputPinNumber.value;
+
+    let accountBalanceValue = parseFloat(accountBalance.innerText);
+
+    // console.log(accountBalanceValue);
+    // console.log(typeof accountBalanceValue);
+
+    
+    // console.log(valueAddMoney, valuePinNumber);
+
+    if(valuePinNumber === '12345')
+    {
+        if(valueAddMoney <= 20000 && valueAddMoney >= 0)
+        {
+            console.log("Money added");
+            const newBalance = accountBalanceValue + valueAddMoney;
+
+            accountBalance.innerText = newBalance;
+            console.log(typeof(accountBalance.innerText));
+
+            // console.log(accountBalanceValue);
+        }
+        else{
+            alert("Amount is not valid");
+        }
+    }
+    else{
+        console.log("Pin is incorrect");
+    }
+
+
+    
+
+});
+
+
+
+
+cashOutBtn.addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    accountBalance.innerText = '0';
+
+
+});
+
+
+
+
+
