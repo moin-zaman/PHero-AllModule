f9700fdfdc011c49cdb178155ea6f6e2f4a43f87
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-19 20:42:25 +0600

    Add Money Modified

--- Milestone_5/Module_28/Module_28_3/js/addMoney.js
+++ Milestone_5/Module_28/Module_28_3/js/addMoney.js
@@ -30,9 +30,26 @@ addMoneyBtn.addEventListener('click', function(event){
 
     const pinNumber = getInputFieldValueByID('input-pin');
 
-    
 
     console.log("Deposite Amount", addMoney);
     console.log("Pin Number", pinNumber);
+
+    if(pinNumber === 12345)
+    {
+        const balance = getTextFieldValueById('account-balance');
+        
+        if(balance >= 0 && balance <= 50000)
+        {
+            const totalBalance = addMoney + balance;
+            document.getElementById('account-balance').innerText = totalBalance;
+        }
+        else{
+            console.log("You can only add 50000 only at a time");
+        }
+    }
+    else
+    {
+        alert("Pin number is not Currect");
+    }
 
 });
