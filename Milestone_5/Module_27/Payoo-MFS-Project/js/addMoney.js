def578d56107bb30bf93f4e8899581ce5b20e136
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-19 14:24:35 +0600

    Add Money Js file Added

---
+++ Milestone_5/Module_27/Payoo-MFS-Project/js/addMoney.js
@@ -0,0 +1,58 @@
+const inputAddMoney = document.getElementById('input-add-money');
+
+const inputPinNumber = document.getElementById('input-pin-number');
+
+const addMoneyBtn = document.getElementById('btn-add-money');
+
+const accountBalance = document.getElementById('account-balance');
+
+
+
+
+addMoneyBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Value get from input field;
+
+    const valueAddMoney = parseFloat(inputAddMoney.value);
+    const valuePinNumber = inputPinNumber.value;
+
+    // Convert Account value value to double
+    let accountBalanceValue = parseFloat(accountBalance.innerText);
+
+    // console.log(accountBalanceValue);
+    // console.log(typeof accountBalanceValue);
+
+
+    // console.log(valueAddMoney, valuePinNumber);
+
+    if (valuePinNumber === '12345') {
+        if (valueAddMoney <= 20000 && valueAddMoney >= 0) {
+            console.log("Money added");
+            const newBalance = accountBalanceValue + valueAddMoney;
+
+            accountBalance.innerText = newBalance;
+            // console.log(typeof (accountBalance.innerText));
+
+            // console.log(accountBalanceValue);
+
+            inputAddMoney.value = '';
+            inputPinNumber.value = '';
+
+        }
+        else {
+            alert("Amount is not valid");
+            inputAddMoney.value = '';
+            inputPinNumber.value = '';
+        }
+    }
+    else {
+        console.log("Pin is incorrect");
+        inputAddMoney.value = '';
+        inputPinNumber.value = '';
+    }
+
+
+
+
+});
