d5f7bb2a8e1a8673d5385955d492f97a6b1b2c84
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-19 10:23:17 +0600

    Modified login.js

--- Milestone_5/Module_27/Payoo-MFS-Project/js/login.js
+++ Milestone_5/Module_27/Payoo-MFS-Project/js/login.js
@@ -8,11 +8,31 @@ const pinNumber = document.getElementById('pin-number');
 
 buttonLogin.addEventListener('click', function(event){
     // step-2: prevent default behavior (prevent reloading browser)
+
     event.preventDefault(); //........ Bhejal line
     console.log("Login Button clicked");
 
     // step-3: get the phone number and the pin number
 
-    console.log(phoneNumber.value);
-    console.log(pinNumber.value);
+    // console.log(phoneNumber.value);
+    // console.log(pinNumber.value);
+    console.log(phoneNumber.value, pinNumber.value);
+
+    // step-4: validate phone and Pins
+    // this is temporary. You should do like this.
+
+    if(phoneNumber.value === '5' && pinNumber.value === '1234')
+    {
+        console.log("You are Logged in");
+        window.location.href = './home.html'
+
+    }
+    else{
+        alert("Wrong phone number or pin");
+    }
+
+    // setting the value of input field as empty
+    // phoneNumber.value = '';
+    // pinNumber.value = '';
+
 });
