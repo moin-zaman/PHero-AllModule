fadc98ba7ac1e8280d76e82b1118be40316e04a4
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-19 19:19:03 +0600

    Get input field value using function

--- Milestone_5/Module_28/Module_28_3/js/addMoney.js
+++ Milestone_5/Module_28/Module_28_3/js/addMoney.js
@@ -1,17 +1,38 @@
 const addMoneyBtn = document.getElementById('add-money-btn');
 
 
-console.log("add Money file loaded");
+
+// addMoneyBtn.addEventListener('click', function(event){
+
+//     // event.preventDefault();
+
+//     // console.log("Add Money Button Clicked");
+
+//     // const inputFieldValues = getInputFieldValueByID();
+//     // console.log("Add money value", inputFieldValues[0]);
+//     // console.log("Pin value", inputFieldValues[1]);
+
+//     // const inputAmount = document.getElementById('input-amount');
+//     // const inputPin = document.getElementById('input-pin');
+
+//     // const balance = inputAmount.value;
+//     // const pin = inputPin.value;
+
+//     // console.log(balance);
+//     // console.log(pin);
+
+// });
+
 
 addMoneyBtn.addEventListener('click', function(event){
 
-    const inputAmount = document.getElementById('input-amount');
-    const inputPin = document.getElementById('input-pin');
+    const addMoney = getInputFieldValueByID('input-amount');
 
-    const balance = inputAmount.value;
-    const pin = inputPin.value;
+    const pinNumber = getInputFieldValueByID('input-pin');
 
-    console.log(balance);
-    console.log(pin);
+    
+
+    console.log("Deposite Amount", addMoney);
+    console.log("Pin Number", pinNumber);
 
 });
