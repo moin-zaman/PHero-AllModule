70af9333dc4d250f8f0807838028e17a8cea73ec
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-24 05:17:03 +0600

    Problem1.js file Added

---
+++ Milestone_6/Module_30/problem1.js
@@ -0,0 +1,21 @@
+// 1)
+const multiplication = (a, b, c) => a * b * c;
+
+const multi = multiplication(4, 7, 3);
+console.log(multi);
+
+
+
+// 2)
+const lines = `I am a web developer.
+I love to code.
+I love to eat biriyani.
+`;
+console.log(lines);
+
+
+
+// 3)
+const addition = (n1, n2 = 5) => n1 + n2;
+const add = addition(2);
+console.log(add);
