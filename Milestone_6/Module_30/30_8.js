10f6cf144d88590d70f86d7fda405cd839dbb75a
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-23 22:30:44 +0600

    Added 30_8.js

---
+++ Milestone_6/Module_30/30_8.js
@@ -0,0 +1,40 @@
+const glass = { 
+    name: 'glass', 
+    color: 'golden', 
+    price: 12, 
+    isCleaned: true 
+};
+
+console.log(glass);
+
+const keys = Object.keys(glass);
+console.log(keys); 
+// [ 'name', 'color', 'price', 'isCleaned' ]
+
+const values = Object.values(glass);
+console.log(values); 
+// [ 'glass', 'golden', 12, true ]
+
+const entries = Object.entries(glass);
+console.log(entries);
+
+// It will return a two dimensional array
+
+// delete glass.isCleaned;
+// console.log(glass);
+
+// Delete using spread operator
+
+const {isCleaned, ...shortGlass} = glass;
+console.log(shortGlass);
+
+// freeze 
+// Object.freeze(glass); // It will allow delete, update, add to object ;
+Object.seal(glass);  // It will allow the update of property value
+glass.source = 'Bangladesh';
+glass.price = 5000;
+delete glass.name;
+console.log(glass);
+
+
+
