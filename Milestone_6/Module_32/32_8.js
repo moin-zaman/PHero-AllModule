20882c3eec23f6c3a925076c44ef886c52bc383b
moin-zaman <moinkhan758abzx@gmail.com>
2025-03-28 19:11:57 +0600

    Modified 32_8

--- Milestone_6/Module_32/32_8.js
+++ Milestone_6/Module_32/32_8.js
@@ -1,16 +1,19 @@
 // primitive types are pass by value
 
-// let num1 = 5;
-// let num2 = 7;
+let num1 = 5;
+let num2 = 7;
 
-// function multiply(a, b){
-//     a = 27;
-//     const result = a * b;
-//     return result;
-// }
+function multiply(a, b){
+    a = 27;
+    // console.log(arguments[1]);
+    const result = a * b;
+    return result;
+    
+}
 // console.log(num1);
 // multiply(num1, num2);
 // console.log(num1);
+multiply(5, 6);
 
 // object and array are pass by reference
 let student1 = {name: 'Jalil', partner: 'borsha'};
@@ -24,3 +27,4 @@ function makeMovie(couple1, couple2){
 console.log(student1);
 makeMovie(student1, student2);
 console.log(student1, student2);
+
